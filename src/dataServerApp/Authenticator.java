daa93a4c8c1cca12b902c76ba6e908e295a2721c
Chengeng Liu <c.liu75@student.unimelb.edu.au>
2019-09-24 12:59:43 +1000

    Add Authenticator Documentation

--- src/dataServerApp/Authenticator.java
+++ src/dataServerApp/Authenticator.java
@@ -15,11 +15,29 @@ public class Authenticator {
     private static String FAIL_HEADER = "Fail";
     private static String SUCCESS_HEADER = "Success";
 
+    private static String USER_NULL= "User name not provided";
+    private static String PASSWORD_NULLL= "Password not provided";
+
+
     public Authenticator(String username, String password){
+        passbook = new HashMap<String, String>();
 //        this.username = username;
 //        this.password = password;
     }
+
+    /**
+     * Register new user. Check if the username or password null first.
+     * @param username  New username that the user wants to add.
+     * @param password  New password that the user wants to add.
+     * @return
+     */
     public JSONObject registerUser(String username, String password){
+        if username == null{
+            return jsonParse(FAIL_HEADER, USER_NULL);
+        }
+        if password == null {
+            return jsonParse(FAIL_HEADER, PASSWORD_NULLL);
+        }
         if (password.contains(username)){
             return jsonParse(FAIL_HEADER, USER_REGISTER_DUPLICATION);
         }
@@ -29,6 +47,13 @@ public class Authenticator {
         }
     }
 
+    /**
+     * Authenticate the user login. Check if the user exist first, then check if the
+     * username and password are previously stored in the passbook.
+     * @param username  New username that the user wants to add.
+     * @param password  New password that the user wants to add.
+     * @return
+     */
     public JSONObject authenticate(String username, String password){
         // If passbook not contain the user name
         if (!password.contains(username)){
@@ -43,7 +68,7 @@ public class Authenticator {
             return jsonParse(SUCCESS_HEADER,USER_AUTHENTICATION_SUCCESS);
         }
     }
-    @SuppressWarnings("unchecked")
+
     private JSONObject jsonParse(String header, String message){
         JSONObject object = new JSONObject();
         object.put(header, message);
