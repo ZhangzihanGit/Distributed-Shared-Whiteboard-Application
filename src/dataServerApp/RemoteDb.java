e62381a999ca41afbd9b32eb6144230b269f391b
Chengeng Liu <c.liu75@student.unimelb.edu.au>
2019-10-05 22:16:04 +1000

    Add java doc

--- src/dataServerApp/RemoteDb.java
+++ src/dataServerApp/RemoteDb.java
@@ -17,6 +17,14 @@ public class RemoteDb extends UnicastRemoteObject implements IRemoteDb {
         this.authenticator = facade.getAuthenticator();
     }
 
+    /**
+     * Register a user into the database.
+     * @param username  Username entered
+     * @param password  Password entered
+     * @param message  假设全部信息都会以一个json格式传过来。
+     * @return
+     * @throws RemoteException
+     */
     @Override
     public JSONObject addUser(String username, String password, JSONObject message) throws RemoteException {
         JSONObject returnMessage = (JSONObject) authenticator.
@@ -28,6 +36,16 @@ public class RemoteDb extends UnicastRemoteObject implements IRemoteDb {
 
         return returnMessage;
     }
+
+    /**
+     * Check if a user is authorised to perform actions. This method envokes Authentication module.
+     * The return message will be a string but with json format. The message has: "header": [Success/ Fail],
+     * "message": [message body]
+     * @param username  username that the user entered.
+     * @param password  password that the user entered.
+     * @return  A string with JSON format.
+     * @throws RemoteException
+     */
     // Authenticate the user by using the information stored in Authenticator.
     @Override
     public String checkUser(String username, String password) throws RemoteException {
@@ -35,6 +53,14 @@ public class RemoteDb extends UnicastRemoteObject implements IRemoteDb {
         System.out.println(returnMessage);
         return returnMessage.toJSONString();
     }
+
+    /**
+     *  Save the cavas to local file system. Under the directory(with the name of the manager)
+     * @param managerName   The manager name that wants to associate the canvas with.
+     * @param wbContent   content of whiteboard(Should be a canvas object? or a png? or a JSON?)
+     * @return  Probably return a save success JSON string? Not too sure yet.
+     * @throws RemoteException
+     */
     // TODO: 输出值根据Web Server决定。
     @Override
     public String saveWb(String managerName, String wbContent) throws RemoteException {
@@ -44,6 +70,13 @@ public class RemoteDb extends UnicastRemoteObject implements IRemoteDb {
         facade.getDataServer().saveCanvas(message, "world");
         return null;
     }
+
+    /**
+     * Load canvas with specified manager name. This manager could possibly hold several canvas(maybe?)
+     * @param managerName
+     * @return
+     * @throws RemoteException
+     */
     // TODO: 输出值根据Web Server决定。 需要看能否存入Canva的东西。
     @Override
     public String loadAllWb(String managerName) throws RemoteException {
