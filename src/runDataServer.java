7eea7a9759422335087766fd35c1fc97437aa44e
Zihan ZHANG <zhazz@student.unimelb.edu.au>
2019-10-22 18:03:41 +1100

    fix back button in signup view; add updateLogger()

--- src/runDataServer.java
+++ src/runDataServer.java
@@ -1,18 +1,14 @@
 import dataServerApp.DataServerApplication;
 import dataServerApp.DataServerFacade;
 
-import java.net.InetAddress;
-import java.net.NetworkInterface;
 import java.net.SocketException;
-import java.net.UnknownHostException;
-import java.util.Enumeration;
 
 public class runDataServer {
     public static void main(String[] args) throws SocketException {
         // log setting
         System.setProperty("my.log", "resources/log/dataServer.log");
         // rmi setting
-        System.setProperty("java.rmi.server.hostname", "10.12.43.225");
+        // System.setProperty("java.rmi.server.hostname", "10.12.43.225");
         // security settings
         System.setProperty("java.security.policy", "file:./security.policy");
         if (System.getSecurityManager() == null) {
